5abf949a075a484e2c4342a97b76c7c348adeb9e
PawnKumar753 <pavankumarreddyd15@gmail.com>
2025-10-16 14:49:39 +0530

    User Service is added

--- Services/EurekaService/src/main/java/com/eureka/demo/EurekaServiceApplication.java
+++ Services/EurekaService/src/main/java/com/eureka/demo/EurekaServiceApplication.java
@@ -1,12 +1,19 @@
 package com.eureka.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(EurekaServiceApplication.class);
+
 	public static void main(String[] args) {
+		log.info("Eureka Server Started");
 		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
